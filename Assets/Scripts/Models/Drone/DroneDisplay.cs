55e51bfbb70ff6f73ec9bdee69e30c7acf4d2555
Sam Goldberg <sgoldberg@tailwindbi.com>
2020-02-25 20:56:43 -0600

    Script stuff - temporary, will need heavy reworks but using for POC

---
+++ Assets/Scripts/Models/Drone/DroneDisplay.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Models.Drone;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DroneDisplay : MonoBehaviour
+{
+    public ScriptableDrone scriptDrone;
+
+    public Text DroneName;
+    public Text Health;
+
+    public Image DroneSprite;
+
+    public Text Shield;
+    public Text Inspiration;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DroneName.text = scriptDrone.Name;
+        Health.text = scriptDrone.Health.ToString();
+        DroneSprite.sprite = scriptDrone.DroneSprite;
+        Shield.text = scriptDrone.Shield.ToString();
+        Inspiration.text = scriptDrone.Inspiration.ToString();
+
+    }
+}
---
+++ Assets/Scripts/Models/Drone/ScriptableDrone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Models.Drone
+{
+    [CreateAssetMenu(fileName = "Drone", menuName = "New Drone", order = 1)]
+    public class ScriptableDrone : ScriptableObject
+    {
+        public string Name;
+        public int Health;
+        public int Energy;
+        public int Shield;
+        // Chance to Level, 00.0%
+        public float Inspiration;
+
+        public Sprite DroneSprite;
+
+        public void Print()
+        {
+            Debug.Log(Name + ": Drone is at " + Health + " HP");
+        }
+    }
+}
